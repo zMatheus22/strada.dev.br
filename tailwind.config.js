400fa4b58d32485ca5e075afafb7b73e4ecc5807
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2024-01-31 23:57:54 -0300

    Updating `CSS` with `TailwindCSS`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,md,mdx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
