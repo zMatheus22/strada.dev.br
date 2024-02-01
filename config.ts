79eab18b4c25fb02e477e970e72f008a9c4a0a2f
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2024-02-01 00:02:06 -0300

    Add `config.ts`

---
+++ config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Portfolio | Matheus Vinicyus Strada";
+export const SITE_DESCRIPTION =
+  "Matheus Vinicyus Strada Portfolio website. Website with CV, Projects Section, Social Link.";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
